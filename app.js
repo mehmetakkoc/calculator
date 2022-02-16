9cb71680556e5be3d08de2a357bdbf40e1c70061
makkoc86 <makkoc27@gmail.com>
2022-02-16 17:54:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// variables
+
+const displayBottom = document.querySelector(".display-bottom");
+const displayTop = document.querySelector(".display-top");
+const acBtn = document.querySelector("#ac");
+const plusMinus = document.querySelector("#plus-minus");
+const modulus = document.querySelector("#modulus");
+const numbers = document.querySelectorAll("#numbers");
+// console.log(numbers.length);
+const comma = document.querySelector("#comma");
+
+const division = document.querySelector("#division");
+const multiplication = document.querySelector("#multiplication");
+const extraction = document.querySelector("#extraction");
+const addition = document.querySelector("#addition");
+const equal = document.querySelector("#equal");
+
+// functions
+
+
+
